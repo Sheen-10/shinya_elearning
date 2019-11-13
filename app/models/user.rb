3128b47d70608ad161c3f55cb90ae71c8bb0de5d
Shinya Mori <shinya199905102525@gmail.com>
2019-11-13 12:02:58 +0800

    Created User Setting Edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 class User < ApplicationRecord
 
-before_save { email.downcase! }
+  before_save { email.downcase! }
 
-validates :name, presence: true, length: {maximum: 50 }
+  validates :name, presence: true, length: {maximum: 50 }
 
-EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-validates :email, presence: true, length: { maximum: 255 },
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
                                   format: { with: EMAIL_REGEX },
                                   uniqueness: { case_sensitive: false }
 
-has_secure_password
-validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  mount_uploader :avatar, AvatarUploader
 end
+
