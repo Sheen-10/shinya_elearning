158183768073e57da20d82b4c22c594b3e398be4
Shinya Mori <56685893+Sheen-10@users.noreply.github.com>
2019-11-14 10:36:56 +0800

    Create Following and Followed (#9)
    * Create Following and Followed
    
    * Fixed Following and Followed

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,33 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+
+  has_many :passive_relationships, class_name: "Relationship",
+                                  foreign_key: "followed_id",
+                                  dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :follower, through: :passive_relationships, source: :follower
+
+# 要は自分がfollowerになりますということ
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # すでにフォローしてるなら2回目をする必要ないですよってこと
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+
   mount_uploader :avatar, AvatarUploader
 end
 
