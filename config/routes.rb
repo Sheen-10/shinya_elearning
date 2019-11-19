9311cd87eacfa774ae81c5092902c7a8113afc82
Shinya Mori <shinya199905102525@gmail.com>
2019-11-19 16:37:04 +0800

    Created Layout Admin Categories Words

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
     get '/home', to: 'users#home'
     resources :users
     resources :categories
+    resources :words
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
