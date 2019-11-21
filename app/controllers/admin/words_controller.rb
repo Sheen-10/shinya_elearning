8e62c66ffcb8faaa8434c9f1fc97643104829c9d
Shinya Mori <56685893+Sheen-10@users.noreply.github.com>
2019-11-21 09:26:53 +0800

    Creared admin words choice answer (#17)
    * Creared admin words choice answer
    
    * Fixed admin words choice answer

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,6 +1,30 @@
 class Admin::WordsController < ApplicationController
 
   def index
-    
+    @category = Category.find(params[:category_id])
+    @words = @category.words
   end
+
+  def new
+    @category = Category.find(params[:category_id])
+    @word = @category.words.build
+    3.times{@word.choices.build}
+  end
+
+  def create
+    @category = Category.find(params[:category_id])
+    @word = @category.words.build(word_params)
+    if @word.save
+      flash[:success] = "Create New Word"
+      redirect_to admin_category_words_url
+    else
+      render "new"
+    end
+  end
+
+  private
+    def word_params
+      params.require(:word).permit(:content, choices_attributes: [:id, :content, :isCorrect])
+    end
+
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,6 @@ class Category < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :description, presence: true, uniqueness: true
 
+  has_many :words, dependent: :destroy
+
 end
