d8764070f44fc8c3f99492369f7fef813fda2095
Shinya Mori <shinya199905102525@gmail.com>
2019-11-28 11:48:14 +0800

    Created result page

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :lessons, only: [:show]
   resources :categories, only: [:index]
   resources :users
   resources :relationships, only: [:create, :destroy]
